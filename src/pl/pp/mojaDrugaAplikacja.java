3c53127babde009dcfe51b07aabc8df97ec08b9d
AEH_PP25L_LUKOMSKI_62289 <lukomskimikolaj4@gmail.com>
2025-03-18 22:03:10 +0100

    Zadanie 2.1

---
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -0,0 +1,24 @@
+package pl.pp;
+
+public class mojaDrugaAplikacja {
+    public mojaDrugaAplikacja() {
+    }
+
+    public static void main(String[] args) {
+
+        double x = 10;
+
+        var result = x + x;
+
+        result = x ;
+        System.out.println("x = " + result);
+
+        result = x + x;
+        System.out.println("2x = " + result);
+
+        result = x * x;
+        System.out.println("x^2 = " + result);
+
+
+    }
+}
